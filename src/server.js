c1a268ae67d81e12fe939dd57258f78191f5991b
santanaigot <santanaigoor01@gmail.com>
2025-10-27 15:56:44 -0300

    [improvment] - inserted and list

--- src/server.js
+++ src/server.js
@@ -4,18 +4,31 @@
 /// tambem utilizamos este padrão de importação ESModule onde as importações e exportações utilizam import e export
 // const http = require('http')
 
+
+// aplicação statefull(sempre terá informações sendo armazenada em memória) e aplicação staless(salva informações em dispositivos externos como banco de dados ) 
+
 import http from 'http'
 // dentro do req consigo obter todas as informações que estão chegando da requisição
+
+// memória onde serão armazenado os dados 
+const users = [];
+
 const server = http.createServer((requisicao, response) => {
     const {method, url} = requisicao;
 
     console.log(method, url);
     
     if(method == 'GET' && url == '/users'){
-        return response.end('listagem de usuário')
+        return response.setHeader('Content-Type', 'application/json').end(JSON.stringify(users))
     }
 
     if(method == 'POST' && url == '/users'){
+        users.push({
+            id:1,
+            nome:"Igor",
+            idade: 22
+        })
+        
         return response.end('Cadastro de usuário')
     }
 
