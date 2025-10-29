fc99ad3f21471dfb0dff9b9cfa006a2f7a6e64ac
santanaigot <santanaigoor01@gmail.com>
2025-10-28 22:55:03 -0300

    [improvment] - streams de leitura, streams of transformation and streams writeable

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -21,23 +21,7 @@
 
 import { Readable, Writable, Transform } from "node:stream";
 
-// essa classe contem um método obrigatorio que é o método read.
-class OneToHundredStream extends Readable {
-  index = 1;
-  _read() {
-    //cada vez que o método read for executado irá somar mais 1
-    const i = this.index++;
 
-    setTimeout(() => {
-      if (i > 100) {
-        this.push(null);
-      } else {
-        const buf = Buffer.from(i.toString());
-        this.push(buf);
-      }
-    }, 1000);
-  }
-}
 
 class InverseNumberStream extends Transform{
     _transform(chunk, encoding, callback){
